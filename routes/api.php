dbad5ab0073e9182dc0a844dd61a861492d06160
Aziz Khasyi <azizkhasyi11@gmail.com>
2024-11-27 13:01:50 +0700

    feat: add Role model, migration, and seeder; update User model for role relationship; implement RoleMiddleware for authorization

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,45 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum')->name('user.profile');
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+        Route::get('/profile', [AuthController::class, 'profile'])->name('auth.profile');
+    });
+});
+
+Route::prefix('products')->name('products.')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('index');
+    Route::get('/{id}', [ProductController::class, 'show'])->name('show');
+    Route::get('/search', [ProductController::class, 'search'])->name('search');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/', [ProductController::class, 'store'])->name('store');
+        Route::put('/{id}', [ProductController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ProductController::class, 'destroy'])->name('destroy');
+    });
+});
+
+Route::prefix('cart')->middleware('auth:sanctum')->name('cart.')->group(function () {
+    Route::get('/', [CartController::class, 'index'])->name('index');
+    Route::post('/', [CartController::class, 'store'])->name('store');
+    Route::delete('/{id}', [CartController::class, 'destroy'])->name('destroy');
+});
+
+Route::prefix('transactions')->middleware('auth:sanctum')->name('transactions.')->group(function () {
+    Route::get('/', [TransactionController::class, 'index'])->name('index');
+    Route::post('/', [TransactionController::class, 'store'])->name('store');
+});
